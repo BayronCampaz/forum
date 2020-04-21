fd39a01e81537ae103d91b53befc98aecf7db5a1
BayronCampaz <bayrondaymir@yahoo.es>
2020-04-21 15:02:49 -0500

    Updates

---
+++ precache-manifest.8ebcf45798fdb59679dfe09236e0bd56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b4e7213c4498491b02969a9ac1c968",
+    "url": "/forum/index.html"
+  },
+  {
+    "revision": "c7996139e82ab022c91b",
+    "url": "/forum/static/css/main.e3602987.chunk.css"
+  },
+  {
+    "revision": "a1b914f7f03eb2331e89",
+    "url": "/forum/static/js/2.8db618b5.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/forum/static/js/2.8db618b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7996139e82ab022c91b",
+    "url": "/forum/static/js/main.90ede1d0.chunk.js"
+  },
+  {
+    "revision": "446de4a4fce982e2d4d3",
+    "url": "/forum/static/js/runtime-main.f5eea032.js"
+  }
+]);
